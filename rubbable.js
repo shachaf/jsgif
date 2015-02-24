fc19277f3e2f175e578b51b8e0f1cbe6792a30ac
Matthew Hasbach <hasbach.git@gmail.com>
2015-02-23 19:46:49 -0500

    Added AMD and CommonJS support

--- rubbable.js
+++ rubbable.js
@@ -52,88 +52,100 @@
 		vp_t, vp_l, vp_ w, vp_h - top, left, width and height of the viewport
 
 */
-var RubbableGif = function( options ) {
-	var sup = new SuperGif( options );
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['./libgif'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('./libgif'));
+    } else {
+        root.RubbableGif = factory(root.SuperGif);
+    }
+}(this, function (SuperGif) {
+    var RubbableGif = function( options ) {
+        var sup = new SuperGif( options );
 
-	var register_canvas_handers = function () {
+        var register_canvas_handers = function () {
 
-			var isvp = function(x) {
-				return (options.vp_l ? ( x - options.vp_l ) : x );
-			}
+            var isvp = function(x) {
+                return (options.vp_l ? ( x - options.vp_l ) : x );
+            }
 
-			var canvas = sup.get_canvas();
-			var maxTime = 1000,
-				// allow movement if < 1000 ms (1 sec)
-				w = ( options.vp_w ? options.vp_w : canvas.width ),
-				maxDistance = Math.floor(w / (sup.get_length() * 2)),
-				// swipe movement of 50 pixels triggers the swipe
-				startX = 0,
-				startTime = 0;
+            var canvas = sup.get_canvas();
+            var maxTime = 1000,
+            // allow movement if < 1000 ms (1 sec)
+                w = ( options.vp_w ? options.vp_w : canvas.width ),
+                maxDistance = Math.floor(w / (sup.get_length() * 2)),
+            // swipe movement of 50 pixels triggers the swipe
+                startX = 0,
+                startTime = 0;
 
-			var cantouch = "ontouchend" in document;
+            var cantouch = "ontouchend" in document;
 
-			var aj = 0;
-			var last_played = 0;
+            var aj = 0;
+            var last_played = 0;
 
-			canvas.addEventListener((cantouch) ? 'touchstart' : 'mousedown', function (e) {
-				// prevent image drag (Firefox)
-				e.preventDefault();
-				if (sup.get_auto_play()) sup.pause();
+            canvas.addEventListener((cantouch) ? 'touchstart' : 'mousedown', function (e) {
+                // prevent image drag (Firefox)
+                e.preventDefault();
+                if (sup.get_auto_play()) sup.pause();
 
-				var pos = (e.touches && e.touches.length > 0) ? e.touches[0] : e;
+                var pos = (e.touches && e.touches.length > 0) ? e.touches[0] : e;
 
-				var x = (pos.layerX > 0) ? isvp(pos.layerX) : w / 2;
-				var progress = x / w;
+                var x = (pos.layerX > 0) ? isvp(pos.layerX) : w / 2;
+                var progress = x / w;
 
-				sup.move_to( Math.floor(progress * (sup.get_length() - 1)) );
-				
-				startTime = e.timeStamp;
-				startX = isvp(pos.pageX);
-			});
+                sup.move_to( Math.floor(progress * (sup.get_length() - 1)) );
 
-			canvas.addEventListener((cantouch) ? 'touchend' : 'mouseup', function (e) {
-				startTime = 0;
-				startX = 0;
-				if (sup.get_auto_play()) sup.play();
-			});
+                startTime = e.timeStamp;
+                startX = isvp(pos.pageX);
+            });
 
-			canvas.addEventListener((cantouch) ? 'touchmove' : 'mousemove', function (e) {
-				e.preventDefault();
-				var pos = (e.touches && e.touches.length > 0) ? e.touches[0] : e;
+            canvas.addEventListener((cantouch) ? 'touchend' : 'mouseup', function (e) {
+                startTime = 0;
+                startX = 0;
+                if (sup.get_auto_play()) sup.play();
+            });
 
-				var currentX = isvp(pos.pageX);
-				currentDistance = (startX === 0) ? 0 : Math.abs(currentX - startX);
-				// allow if movement < 1 sec
-				currentTime = e.timeStamp;
-				if (startTime !== 0 && currentDistance > maxDistance) {
-					if (currentX < startX && sup.get_current_frame() > 0) {
-						sup.move_relative(-1);
-					}
-					if (currentX > startX && sup.get_current_frame() < sup.get_length() - 1) {
-						sup.move_relative(1);
-					}
-					startTime = e.timeStamp;
-					startX = isvp(pos.pageX);
-				}
+            canvas.addEventListener((cantouch) ? 'touchmove' : 'mousemove', function (e) {
+                e.preventDefault();
+                var pos = (e.touches && e.touches.length > 0) ? e.touches[0] : e;
 
-				var time_since_last_play = e.timeStamp - last_played;
-				{
-					aj++;
-					if (document.getElementById('tickles' + ((aj % 5) + 1))) document.getElementById('tickles' + ((aj % 5) + 1)).play();
-					last_played = e.timeStamp;
-				}
+                var currentX = isvp(pos.pageX);
+                currentDistance = (startX === 0) ? 0 : Math.abs(currentX - startX);
+                // allow if movement < 1 sec
+                currentTime = e.timeStamp;
+                if (startTime !== 0 && currentDistance > maxDistance) {
+                    if (currentX < startX && sup.get_current_frame() > 0) {
+                        sup.move_relative(-1);
+                    }
+                    if (currentX > startX && sup.get_current_frame() < sup.get_length() - 1) {
+                        sup.move_relative(1);
+                    }
+                    startTime = e.timeStamp;
+                    startX = isvp(pos.pageX);
+                }
+
+                var time_since_last_play = e.timeStamp - last_played;
+                {
+                    aj++;
+                    if (document.getElementById('tickles' + ((aj % 5) + 1))) document.getElementById('tickles' + ((aj % 5) + 1)).play();
+                    last_played = e.timeStamp;
+                }
 
 
-			});
-		};
+            });
+        };
 
-	sup.orig_load = sup.load;
-	sup.load = function(callback) {
-		sup.orig_load( function() {
-			if (callback) callback();
-			register_canvas_handers( sup );
-		} );
-	}
+        sup.orig_load = sup.load;
+        sup.load = function(callback) {
+            sup.orig_load( function() {
+                if (callback) callback();
+                register_canvas_handers( sup );
+            } );
+        }
 
-	return sup;
-}
+        return sup;
+    }
+
+    return RubbableGif;
+}));
